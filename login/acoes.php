8557154c9d383110bf6700a3141522ec26abb60d
Daila Santos <111146154+Daila-Danielle@users.noreply.github.com>
2023-01-12 14:02:51 -0300

    Update acoes.php
    foi corrigido para inserir foto padrao caso o usuario não insira uma foto

--- login/acoes.php
+++ login/acoes.php
@@ -1,12 +1,14 @@
 <?php
     switch(@$_REQUEST["acao"]){
         case "cadastrar":  
+            ini_set ( 'display_errors' , 1); error_reporting (E_ALL);
+            
             #pega os dados digitado no formulario.
-
             $nome      = $_POST["nome"];
             $celular   = $_POST["celular"];
             $email     = $_POST["email"];
             $senha     = password_hash($_POST["senha"], PASSWORD_DEFAULT);
+            $foto_perfil = $_FILES['foto_perfil'];
 
             #verifica se tem email já cadastrados se tiver exibe mensagem de erro
             #se não faz o INSERT no banco de dados.
@@ -15,12 +17,15 @@
                 print "<script>location.href='?pagina=cadastro-usuario';</script>";
             }
             else{
-                #pega a foto de perfil do usuario
-                if(isset($_FILES['foto_perfil'])) {
-                    $foto_perfil = $_FILES['foto_perfil'];
+                #se tiver datos no arquivo da foto de perfil eles realiza o processo para salvar a imagem do usuario e inserir todos os dados no banco de dados, se não ele insere uma foto padrao
+                if(empty($foto_perfil['tmp_name'])== False) { #essa função verifica se está vazio ou não se a variavel estiver vazia ela retorna True se estiver preenchida retorna False
+                    print "<script>alert('cheguei aqui');</script>";
+                    
+                    var_dump($foto_perfil);
                     if($foto_perfil['size'] > 2097152) { #se o arquivo da foto for maior de 2mb exibe uma mensagem de arquivo muito grande
                         print "<script>alert('Arquivo muito grande! maximo 2MB');</script>";   
                     }
+                    
                     $pasta = "img-perfil-usuario/"; #pasta onde as imagens serão salvas
                     $nomeDoArquivo = $foto_perfil['name']; #pega o nome do arquivo enviado pelo usuario
                     $novoNomeDoArquivo = uniqid(); #uniqid é uma função que da um nome aleatorio pro arquivo
@@ -28,19 +33,23 @@
 
                     if ($extensao != "jpg" && $extensao != "png" ) { # Se o arquivo for diferente de jpg e png ele exibe uma mensagem de erro 
                         die('Tipo de arquivo não aceito');
+                        
                     }
-
                     $path = $pasta . $novoNomeDoArquivo . "." . $extensao; # onde o arquivo será salvo e o nome como se fosse o href dele
 
-                    $deu_certo = move_uploaded_file($foto_perfil["tmp_name"], $path); # retorna true se deu certo e false se não
+                    $deu_certo = move_uploaded_file($foto_perfil['tmp_name'], $path); # função que pega um arquivo e salva em outro local especificado apo´s a virgula retorna true se deu certo e false se não
+                    var_dump($deu_certo);
                 }
                 else{
                     $path = "img-perfil-usuario/foto_padrao.png";
+                    $deu_certo = True;
+                    
                 }
-                if($deu_certo){
-
+                if($deu_certo==true){
+                    
                     $sql = "INSERT INTO tb_usuario (nome, email, senha, celular, foto_perfil) VALUES ('{$nome}', '{$email}', '{$senha}', '{$celular}','{$path}')";
                     $res  = $conexao->query($sql);
+                    
                     if ($res==true){
                         print "<script>alert('Cadastrado com Sucesso!');</script>";
                         print "<script>location.href='?pagina=login';</script>";
@@ -50,10 +59,10 @@
                         print "<script>location.href='?pagina=cadastro-usuario';</script>";
                     }
 
+                }else{
+                    print "<script>alert('cheguei aqui');</script>";
                 }
-                
             }
-            
             
         break;
         case "verificar":
